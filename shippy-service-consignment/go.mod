15b85e5eb4994b8bf13da844b82318b5969ace59
Andres Koga <a20160037@pucp.edu.pe>
2021-03-16 13:59:46 -0500

    Changes

--- shippy-service-consignment/go.mod
+++ shippy-service-consignment/go.mod
@@ -1,5 +1,3 @@
 module github.com/KKogaa/shippy/shippy-service-consignment
 
 go 1.15
-
-require google.golang.org/grpc v1.36.0
--- shippy-service-consignment/main.go
+++ shippy-service-consignment/main.go
@@ -8,7 +8,7 @@ import (
 	"sync"
 
 	// Import the generated protobuf code
-	pb "github.com/KKogaa/shippy-service-consignment/proto/consignment"
+	pb "github.com/KKogaa/shippy/shippy-service-consignment/proto/consignment"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
 )
